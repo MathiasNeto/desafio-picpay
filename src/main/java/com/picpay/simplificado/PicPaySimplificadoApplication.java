2165f2e487fffc1b9c0cbb40d8e35e7fa96d8d15
Emanuel Matias <matias.neto.edu@gmail.com>
2024-02-29 01:22:10 -0300

    first commit

---
+++ src/main/java/com/picpay/simplificado/PicPaySimplificadoApplication.java
@@ -0,0 +1,14 @@
+package com.picpay.simplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class PicPaySimplificadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicPaySimplificadoApplication.class, args);
+    }
+
+}
